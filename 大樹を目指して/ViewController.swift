3f2e1e35a676bb9d60a4d908d8059b8ac7e36eb9
k-saito-en <saito.kotaro.engineer@gmail.com>
2022-11-20 09:32:54 +0900

    Initial Commit

---
+++ 大樹を目指して/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  大樹を目指して
+//
+//  Created by 齊藤虎太郎 on 2022/11/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ 大樹を目指してUITests/_______UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  _______UITestsLaunchTests.swift
+//  大樹を目指してUITests
+//
+//  Created by 齊藤虎太郎 on 2022/11/20.
+//
+
+import XCTest
+
+final class _______UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
